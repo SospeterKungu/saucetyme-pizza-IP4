6871c7058f258f6c0d4a8bac536a1fd78153bec5
Sospeter Kungu <sospeter2030@gmail.com>
2020-05-15 13:14:04 +0300

    formatted the index.html using styles.css to enhance page appearance

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     $("#dotext1").hide();
     $("#dotext2").hide();
     $("#dotext3").hide();
+    $("#dotext4").hide();
      $(".imagetoggle0 , .imagetoggle1").click(function(){
      $("#dotext").toggle();
      $(".imagetoggle0").toggle();
@@ -13,5 +14,9 @@ $(document).ready(function(){
      $(".imagetoggle4 , .imagetoggle5").click(function(){
      $("#dotext3").toggle();
      $(".imagetoggle4").toggle();
+     });
+     $(".imagetoggle6 , .imagetoggle7").click(function(){
+     $("#dotext4").toggle();
+     $(".imagetoggle6").toggle();
      });
    });
